6b615ebe9bb7956c1f53dadc1fddf74825bcef02
elfsvet <stepanmatysik@gmail.com>
2022-03-27 10:52:34 -0400

    finished product with one problem and need to do readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-
 // all characters what we can use in an object or i can make arrays it would be better performance
 var charactersType = {
   upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
@@ -8,93 +7,61 @@ var charactersType = {
   specialCharacters: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~']
 };
 
-// choose a random character from an array/ needs provide an array as an argument
+// choose a random character from an array/it needs to provide an array as an argument
 var randomCharacterFromArray = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
-// a choice of a single character from an array
-var randomUpperCase = randomCharacterFromArray(charactersType.upperCase);
-var randomLowerCase = randomCharacterFromArray(charactersType.lowerCase);
-var randomNumber = randomCharacterFromArray(charactersType.numbers);
-var randomSpecialCharacter = randomCharacterFromArray(charactersType.specialCharacters);
-
-
-// var randomUpperCase = randomCharacterFromArray(charactersType.upperCase);
-// var randomLowerCase = randomCharacterFromArray(charactersType.lowerCase);
-// var randomNum = randomNumber(0,10);
-// var randomSpecialCharacter = randomCharacterFromArray(charactersType.specialCharacters);
-
-
 
 var chooseConfirm = function () {
-  debugger;
+  
   // we need generate an array with all accepted characters/letters/numbers
   var arrayOfCharacters = [];
   // confirm if a user wants: uppercase
-  var upperCaseAnswer = confirm("Do you want your password to contain Uppercase letters?");
+  var confirmUpperCase = confirm("Do you want your password to contain Uppercase letters?");
   // confirm if a user wants: lowercase
-  var lowerCaseAnswer = confirm("Do you want your password to contain Lowercase letters?");
+  var confirmLowerCase = confirm("Do you want your password to contain Lowercase letters?");
   // confirm if a user wants: numbers
-  var numberAnswer = confirm("Do you want your password to contain numbers?");
+  var confirmNumbers = confirm("Do you want your password to contain numbers?");
   // confirm if a user wants: special characters
-  var specialCharactersAnswer = confirm("Do you want your password to contain special characters?");
+  var confirmSpecialCharacters = confirm("Do you want your password to contain special characters?");
+  
   // if all answers No a user need to choose at least one option to have a password
-  if (!upperCaseAnswer && !lowerCaseAnswer && !numberAnswer && !specialCharactersAnswer) { // we might try while later!!!!!!!!!!!
+  if (!confirmUpperCase && !confirmLowerCase && !confirmNumbers && !confirmSpecialCharacters) { // we might try while later!!!!!!!!!!!
     window.alert("You need to choose at least one option, or you will not get anything.");
-    // debugger;
-    chooseConfirm();
-  } else {
-    // DRY
-    var addToArray = function (answer, arrayToAdd) {
-      if (answer) {
-        arrayOfCharacters = arrayOfCharacters.concat(arrayToAdd);
-      }
-      return arrayOfCharacters;
-    };
-    // if answer of a user OK updates array and return it for future random pull from it.
-    // if (upperCaseAnswer) {
-      //   arrayOfCharacters = addToArray(arrayOfCharacters, upperCaseAnswer, charactersType.upperCase);
-      // }
-      // if (lowerCaseAnswer) {
-        //   arrayOfCharacters = addToArray(arrayOfCharacters, lowerCaseAnswer, charactersType.lowerCase);
-        // }
-        // if (numberAnswer) {
-          //   arrayOfCharacters = addToArray(arrayOfCharacters, numberAnswer, charactersType.numbers);
-          // }
-          // if (specialCharactersAnswer) {
-            //   arrayOfCharacters = addToArray(arrayOfCharacters, specialCharactersAnswer, charactersType.specialCharacters);
-            // }
-            
-    arrayOfCharacters = addToArray(upperCaseAnswer, charactersType.upperCase);
-    arrayOfCharacters = addToArray(lowerCaseAnswer, charactersType.lowerCase);
-    arrayOfCharacters = addToArray(numberAnswer, charactersType.numbers);
-    arrayOfCharacters = addToArray(specialCharactersAnswer, charactersType.specialCharacters);
+    chooseConfirm();// if we mute it, all works perfectly
+
   }
+  // DRY
+  var addToArray = function (answer, arrayToAdd) {
+    if (answer) {
+      arrayOfCharacters = arrayOfCharacters.concat(arrayToAdd);
+    }
+    return arrayOfCharacters;
+  };
+  // we need to add all selected characters in to an array to choose from.
+  arrayOfCharacters = addToArray(confirmUpperCase, charactersType.upperCase);
+  arrayOfCharacters = addToArray(confirmLowerCase, charactersType.lowerCase);
+  arrayOfCharacters = addToArray(confirmNumbers, charactersType.numbers);
+  arrayOfCharacters = addToArray(confirmSpecialCharacters, charactersType.specialCharacters);
   console.log(arrayOfCharacters); // prints correct array.
   //something weird is going on. return runs and returns back to its position  jumps back to return with no values of the true what i input before.RIGHT HERE
   return arrayOfCharacters;
   // return the array we will pick up our characters from
 };
 
-// we need to add all selected characters in to an array to choose from.
-
-
-
 var generatePassword = function () {
   //we would need a string to assign result to 
   var readyPassword = "";
-
   // ask what the length a user wants their password to be. From 8 - 128 characters and check if user entered numbers if it's something else it will return NaN, and float will be whole number/ does wrong with fractions MAY NEED TO USE A RULE WITH USING '/'
-  var amountOfCharacters = parseInt(window.prompt("How many characters would you like your password to be?\nChoose between 8 and 128 characters."));
+  var lengthOfPassword = parseInt(window.prompt("How many characters would you like your password to be?\nChoose between 8 and 128 characters."));
   // if a user did not input any information or the 8>length of password>128
-  if (!amountOfCharacters || amountOfCharacters < 8 || amountOfCharacters > 128) {
+  if (!lengthOfPassword || lengthOfPassword < 8 || lengthOfPassword > 128) {
     window.alert("Wrong input, try again.");
-    // debugger;
-    generatePassword();
+    generatePassword(); // if we mute it, all works perfectly
   } else { // if all correct with input for the length.
     var answer = [];
     answer = chooseConfirm();
-    for (var i = 0; i < amountOfCharacters; i++) {
+    for (var i = 0; i < lengthOfPassword; i++) {
       readyPassword += randomCharacterFromArray(answer);
     }
   }
@@ -108,7 +75,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
